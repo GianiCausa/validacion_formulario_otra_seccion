7f6f472257a7ab5717d17364fce55c4ca794d4c1
gianinna <gi.causa@duocuc.cl>
2024-04-25 16:38:16 -0400

    formulario base

---
+++ assets/js/form.js
@@ -0,0 +1,11 @@
+var rut = document.getElementById("rut");
+var nombre = document.getElementById("nombre");
+var ap_paterno = document.getElementById("ap_paterno");
+var ap_materno = document.getElementById("ap_materno");
+var fecha = document.getElementById("fecha");
+var edad = document.getElementById("edad");
+var genero = document.getElementById("genero");
+var email = document.getElementById("email");
+var celular = document.getElementById("celular");
+var profesion = document.getElementById("profesion");
+
